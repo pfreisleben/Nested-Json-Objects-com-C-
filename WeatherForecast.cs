ee7c3f6650f34d749cbea8bdf44a20929b28659a
pfreisleben <pedrofreislebeen@gmail.com>
2022-03-10 16:54:31 -0300

    não permite informar nenhum valor no campo TemperatureC

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -1,15 +1,15 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace webapiTeste
 {
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
-
+        [Range(1, int.MaxValue, ErrorMessage = "É necessário informar algum valor")]
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string Summary { get; set; }
 
         public Teste? Teste { get; set; }
