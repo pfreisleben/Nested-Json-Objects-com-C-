f7e8041d0420a108202e97d9a8cf97190c99470d
pfreisleben <pedrofreislebeen@gmail.com>
2022-03-10 15:48:42 -0300

    Adicionar arquivos de projeto.

---
+++ WeatherForecast.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace webapiTeste
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+
+        public Teste Teste { get; set; }
+    }
+
+    public  class Teste
+    {
+        public int Teste1 { get; set; }
+
+        public int Teste2 { get; set; }
+    }
+}
